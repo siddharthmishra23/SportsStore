0bf123e9c171dd044141d5aff139787c3244d143
siddharth mishra <smsiddharthmishra@gmail.com>
2024-09-14 13:05:13 +1000

    Initital setup

---
+++ SportsStore/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+ 
+@Component({
+  selector: "app",
+  template: `<div class="bg-success p-2 text-center text-white">
+                This is SportsStore
+             </div>`
+})
+export class AppComponent { }
